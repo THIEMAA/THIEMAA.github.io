b37dcdae9c70460e6d034585760e7c696932f86c
THIEMAA <thiagorodriguez91@gmail.com>
2023-08-14 15:47:09 -0300

    cambio de class a img

--- Trabajos/main.js
+++ Trabajos/main.js
@@ -1,4 +1,17 @@
-const image = document.querySelector('img');
-const titulo = document.queryselector('h3');
-const descripcion = document.queryselector('p');
 
+    const imagen = document.querySelector('img');
+    const titulo = document.querySelector('h3');
+    const descripcion = document.querySelector('p');
+    const precio = document.querySelector('small');
+
+    const URL = 'https://fakestoreapi.com/products';
+    
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+
+            titulo.innerHTML = data[0].title;
+            imagen.src = data[0].image;
+            descripcion.innerHTML = data[0].description;
+            precio.innerHTML = data[0].price;            
+})
